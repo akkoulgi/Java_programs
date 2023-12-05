543eccc38d5f5dc0583a526189307cb622ac035b
akoul <akshay.koulgi@gmail.com>
2023-12-05 23:45:06 +0530

    new line added

--- src/Basic_programs/Basics.java
+++ src/Basic_programs/Basics.java
@@ -4,15 +4,13 @@ public class Basics {
 
 	public static void main(String[] args) {
 			
-		System.out.println(40+40); // 80
-		System.out.println("the value is"+20); // the value is 20
-		System.out.println("the value is"+20+20); // the value is 40
-		System.out.println(20+"is the value"); // 20 is the value
 		
 		System.out.println(+20+"is the value"); // 20 is the value
 		System.out.println(+20+"is the value"); // 20 is the value
 		System.out.println(" value is "+(20+20)); // 20 is the value
 
+		
+		System.out.println("new line");
 	}
 
 }
